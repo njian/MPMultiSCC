554352ba1d7fb199191d48e694dff540c6f9f98b
njian <nj227@cornell.edu>
2017-05-23 05:51:22 -0400

    Discard infeasible soln in beta step
    when SL < alpha, the beta step should fail

--- bisection_beta.m
+++ bisection_beta.m
@@ -1,4 +1,4 @@
-function [f_n, x_ast_ast, SL_n, sd_n, beta_U, n_sim] = bisection_beta(n, runlength, seed, serviceLevelMin, nCallTypes, nAgentGroups, arrivalRates, meanST, R, Route, shifts, aggressiveSearch)
+function [f_n, x_ast_ast, SL_n, sd_n, beta_U, n_sim, fAll_beta] = bisection_beta(n, runlength, seed, serviceLevelMin, nCallTypes, nAgentGroups, arrivalRates, meanST, R, Route, shifts, aggressiveSearch)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 epsilon = 0.1; % stopping precision for beta
@@ -8,6 +8,10 @@ n_sim = 0;
 
 count_beta = 0;
 f_n = 1e4;
+x_ast_ast = NaN;
+SL_n = 0;
+sd_n = 0;
+fAll_beta = []
 
 % nShifts = size(shifts, 1);
 % x_trial = zeros(nAgentGroups, nShifts);      
@@ -44,18 +48,22 @@ while beta_U - beta_L > epsilon
     n_sim = n_sim + reps;
     
     fprintf('======================================================================================== \n');
-    if f_beta < f_n
+    if f_beta < f_n && SL_beta >= serviceLevelMin
         fprintf('beta step: IMPROVE f from %.2f to %.2f +/- %.2f. \n', f_n, f_beta, sd_beta);
         % update the best solution for any beta
         x_ast_ast = x_ast;
         f_n = f_beta;
         SL_n = SL_beta;
         sd_n = sd_beta;
+        cost_n = beta * (SL_n - serviceLevelMin) - f_n;
+        fAll_beta = [fAll_beta, [n; n_sim; f_n; sd_n; cost_n; SL_n; beta]];
         fprintf('Number of agents = %d. SL = %.2f. \n', sum(sum(x_ast_ast)), SL_n);
+    elseif SL_beta < serviceLevelMin
+        fprintf('beta step: Infeasible SL, increasing beta_L from %.2f to %.2f. \n', beta_L, beta);
     else
         fprintf('beta step: failed: this solution %.2f >= last best %.2f. Last SL = %.2f. \n', f_beta, f_n, SL_n);
     end
-
+    
     % Bisection beta step: find beta s.t. SL = serviceLevelMin
     if count_beta == 1 && SL_beta < serviceLevelMin
         fprintf('Bisection of beta failed. SL_beta = %.2f, so n is not big enough for beta_U. \n', SL_beta);
