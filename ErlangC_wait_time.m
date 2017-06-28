8f58364b7d6780250cb805688a00b18eed7d81d3
njian <nj227@cornell.edu>
2017-06-28 00:44:00 -0400

    Fixed bug in ErlangC wait time
    when rho>1, wait time is Inf. When rho=1, model is inconclusive, here
    making it Inf.

--- ErlangC_wait_time.m
+++ ErlangC_wait_time.m
@@ -1,16 +1,19 @@
 function [ avgWait ] = ErlangC_wait_time( arrivalRate, meanST, c )
 % Return the average waiting time of a M/M/c/inf queue with arrivalRate and
-% meanST and c servers
+% meanST and c servers. arrivalRate and meanST should be in the same time
+% unit.
 
 rho = arrivalRate * meanST / c;
-
-total = 0;
-for m = 0:c-1
-    total = total + (c*rho)^m / factorial(m);
+if rho >= 1
+    avgWait = Inf;
+else
+    total = 0;
+    for m = 0:c-1
+        total = total + (c*rho)^m / factorial(m);
+    end
+    P0 = 1/total;
+    avgWait = ((c*rho)^c * rho / factorial(c) / (1-rho)^2 * P0) / arrivalRate;
 end
-total = total + (c*rho)^c / factorial(c) / (1-rho);
-
-avgWait = ((c*rho)^c * rho / factorial(c) / (1-rho)^2 / total) / arrivalRate;
 
 end
 
