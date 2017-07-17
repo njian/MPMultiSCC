a33dfeb2851c101acc444c446b58c508b9f0f7f2
njian <nj227@cornell.edu>
2017-07-17 19:19:50 -0400

    bug fix: costPerDay
    The length l_q is in terms of number of periods, not hours.

--- CostPerDay.m
+++ CostPerDay.m
@@ -7,10 +7,9 @@ for group = 1:length(x(:,1))
     for shift = 1:length(x(1,:))
         if x(group, shift) > 0
             nu = sum(R(group,:) ~= 1000); % number of callTypes that can be handled
-            len = (shifts(shift,5) - shifts(shift,1))/60;
+            len = (shifts(shift,5) - shifts(shift,1))/15;
             CostPerDay(group, shift) = x(group, shift) * (1+(nu-1) * varsigma)*(len/30);
         end
     end
 end
 end
-
